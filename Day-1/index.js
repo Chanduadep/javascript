a956e70f5dd7963ec5c7cef0634363796095c294
Chanduadep <adepchandu22@gmail.com>
2024-07-08 14:08:51 +0530

    updated

---
+++ Day-1/index.js
@@ -0,0 +1,64 @@
+console.log(typeof("chandana"))
+console.log(typeof('bhavana'))
+console.log(typeof(`javascript`))
+console.log(typeof(23))
+console.log(typeof("23"))
+console.log(typeof(true))
+console.log(typeof(false))
+
+var username="xyz";
+console.log(username);
+var userage=20;
+console.log(userage);
+var useremail="xyz@gmail.com";
+console.log(useremail);
+var userpassword="xyz123";
+console.log(userpassword);
+var user;
+console.log(user,typeof(user));
+
+var number1=2345;
+var number2=4567;
+console.log(number1+number2);
+console.log(number1-number2);
+console.log(number1*number2);
+console.log(number1/number2);
+console.log(number1%number2);
+
+var number=12;
+console.log(number);
+number=14;
+console.log(number);
+var number=16;
+console.log(number);
+
+let number=10;
+console.log(number);
+number=12;
+console.log(number);
+let number=14;
+console.log(number);
+
+const number=34;
+console.log(number);
+number=12;
+console.log(number);
+const number=24;
+console.log(number);
+
+{
+    var number=10;
+    let number2=12;
+    console.log(number2);
+    const number3=14;
+}
+console.log(number);
+{
+    var number=10;
+    // const number=13;
+    // let number=14;
+    {
+        console.log(number);
+    }
+}
+console.log(number);
