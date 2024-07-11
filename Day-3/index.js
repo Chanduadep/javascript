d202528af595f96452aedc208ee3b94dc18f570f
Chanduadep <adepchandu22@gmail.com>
2024-07-11 20:09:29 +0530

    switch

---
+++ Day-3/index.js
@@ -0,0 +1,66 @@
+var age=18;
+if(age<18){
+    console.log("You are not eligible for driving licence");
+} else if(age==18){
+    console.log("You are eligible for learning driving licence");
+} else{
+    console.log("You are not eligible for driving licence");
+}
+
+var userselectedMonth = "September";
+switch (userselectedMonth) {
+    case "January":
+        console.log("You selected January");
+        break;
+    case "February":
+        console.log("You selected February");
+        break;
+    case "March":
+        console.log("You selected March");
+        break;
+    case "April":
+        console.log("You selected April");
+        break;
+    case "May":
+        console.log("You selected May");
+        break;
+    case "June":
+        console.log("You selected June");
+        break;
+    case "July":
+        console.log("You selected July");
+        break;
+    case "August":
+        console.log("You selected August");
+        break;
+    case "September":
+        console.log("You selected September");
+        break;
+    case "October":
+        console.log("You selected October");
+        break;
+    case "November":
+        console.log("You selected November");
+        break;
+    case "December":
+        console.log("You selected December");
+        break;
+    default:
+        console.log("You are not selected any Month");
+
+}
+
+var student1=100;
+var student2=50;
+var student3=25;
+
+// console.log(student1>100 || student2>50 || student3<25);
+
+if(student1>=50){
+    console.log("More than 50 marks");
+} else if(student2>=50){
+    console.log("Student2 having more than 50 marks.");
+} else if(student3>=50){
+    console.log("Student3 having more than 50 marks.");
+}
+
