e5461127f95bc478fa4bcb572944cbf620348204
Chanduadep <adepchandu22@gmail.com>
2024-07-15 14:08:10 +0530

    for loop

---
+++ Day-5/index.js
@@ -0,0 +1,20 @@
+// const isNumberisEverorOdd = (number) => {
+//     if(number%2==0){
+//         console.log(`Given Number ${number} is Even`);
+//     } else{
+//         console.log(`Given Number ${number} is Odd`);
+//     }
+// };
+// isNumberisEverorOdd(17);
+
+for(var i=20;i<=40;i++){
+    console.log(i);
+}
+
+for(var i=1;i<=100;i+=2){
+    console.log(i);
+}
+
+for(var i=50;i<=150;i+=10){
+    console.log(i);
+}
