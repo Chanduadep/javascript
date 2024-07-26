2149530c6bc73345fd8fa4a1509dabe81d656c80
Chanduadep <adepchandu22@gmail.com>
2024-07-26 22:52:27 +0530

    updated

--- Day-2/index.js
+++ Day-2/index.js
@@ -1,36 +1,36 @@
-number=10;
-console.log(number);
-// var number;
-// let number;
-// const number;
+// number=10;
+// console.log(number);
+// // var number;
+// // let number;
+// // const number;
 
-console.log(20>30);
-console.log(20<30);
-console.log(20>=30);
-console.log(20<=30);
-console.log(10<=10);
+// console.log(20>30);
+// console.log(20<30);
+// console.log(20>=30);
+// console.log(20<=30);
+// console.log(10<=10);
 
-console.log(10=="10");
+// console.log(10=="10");
 
-console.log(10==="10");
+// console.log(10==="10");
 
-if(false)
-{
-    console.log("if block");
-}else{
-    console.log("else block");
-}
+// if(false)
+// {
+//     console.log("if block");
+// }else{
+//     console.log("else block");
+// }
 
-if(10<20){
-    console.log("10 is less than 20");
-} else{
-    console.log("10 is not less than 20");
-}
+// if(10<20){
+//     console.log("10 is less than 20");
+// } else{
+//     console.log("10 is not less than 20");
+// }
 
-var number1=500;
-var number2=300;
-if(number1<=number2){
-    console.log("500 is not less than 300");
-} else{
-    console.log("500 is less than 300");
-}
+// var number1=500;
+// var number2=300;
+// if(number1<=number2){
+//     console.log("500 is not less than 300");
+// } else{
+//     console.log("500 is less than 300");
+// }
