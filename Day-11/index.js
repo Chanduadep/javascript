7a6572d2ed85d3bbb04282faebeb74c289a0bad1
Chanduadep <adepchandu22@gmail.com>
2024-07-28 11:30:45 +0530

    array methods

---
+++ Day-11/index.js
@@ -0,0 +1,28 @@
+let array=[1,2,3,35,78];
+const output=array.find((ele)=>ele>10);
+console.log(output);
+
+
+
+var student1=[1,2,3,4,5];
+var student2=[7,1,2,7,4,9];
+const ne=student1.concat(student2);
+console.log(ne);
+
+
+var fname=["mahesh","Ram","kiansh","shreya"];
+const names=fname.filter((num)=>num.length>=7);
+console.log(names);
+
+const fname=["mahesh","Ram","kiansh","shreya"]
+fname.forEach((student)=>console.log(student));
+
+const fname=["mahesh","Ram","kiansh","shreya","shreyansh"]
+const stud=fname.includes("shreyansh");
+console.log(stud);
+
+
+ const studname=["rihan","kiara","sneha","pooja"];
+ const students=studname.indexOf("kiara");
+ console.log(students);
+
