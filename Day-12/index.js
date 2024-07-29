e1e14007da5fc7fef4a2fc61d79ddf5f81c1aeff
Chanduadep <adepchandu22@gmail.com>
2024-07-29 13:17:13 +0530

    objects

---
+++ Day-12/index.js
@@ -0,0 +1,89 @@
+// const studentData=
+// {
+//     name:"chandana",
+//     age:19,
+//     isGraduated:true,
+//     grades:["A","A","A","A","A","A+"],
+//     std:{january:"Present",february:"partially"}
+// };
+// console.log(studentData);
+// console.log(studentData.grades[studentData.grades.length-1]);
+
+
+// const myObject=
+// {
+//     name:"mehar",
+//     age:19,
+//     study:"BscIT"
+// };
+
+// console.log(myObject.age);
+// console.log(myObject['study']);
+// myObject.grade="A+";
+// console.log(myObject);
+// myObject['location']="Bhiwandi";
+// console.log(myObject);
+
+
+// const student = new Object();
+// student.name = "Dhaval";
+// student.surname = "abc";
+// student['age'] = 20;
+// console.log(student);
+
+// const myObject=
+// {
+//     name:"mehar",
+//     age:19,
+//     study:"BscIT"
+// };
+// for(let key in myObject){
+//     console.log(key,myObject[key]);
+// }
+
+
+// console.log(Object.keys(myObject));
+// console.log(Object.values(myObject));
+
+// Q Given an string find out numbers of repetation for single lette.
+
+// var string="chandana";
+// function findDuplicates(string){
+//     var output={};
+//     // console.log(string,"string");
+//     for(let i=0;i<string.length;i++){
+//         // console.log(string[i]);
+//         if(output[string[i]]== undefined){
+//             output[string[i]]=1;
+//         }else{
+//             output[string[i]]++;
+//         }
+//     }
+//     console.log(output);
+// }
+// findDuplicates(string);
+
+//Q Given an string find out numbers of repetation for single letter and return a letter which having most of reperation .
+
+var string="Balalaxmi";
+function findDuplicates(string){
+    var output={};
+    var largeCount=0;
+    var repeatedletter='';
+    // console.log(string,"string");
+    for(let i=0;i<string.length;i++){
+        // console.log(string[i]);
+        if(output[string[i]]== undefined){
+            output[string[i]]=1;
+        }else{
+            output[string[i]]++;
+        }
+        if(output[string[i]]>largeCount){
+            largeCount=output[string[i]];
+            repeatedletter=string[i];
+        }
+    }
+    // console.log(output);
+    console.log(repeatedletter);
+}
+findDuplicates(string);
